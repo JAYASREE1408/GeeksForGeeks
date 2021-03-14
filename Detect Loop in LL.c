927dc5b0b36531971cf00c6a69e2342ef8f9d81b
JAYASREE T <jayasreet2112@gmail.com>
2021-03-14 22:05:02 +0530

    Detect Loop in LL

---
+++ Detect Loop in LL.c
@@ -0,0 +1,17 @@
+class Solution
+{
+    public:
+    bool detectLoop(Node* head)
+    {
+        struct Node*slow,*fast;
+        slow=fast=head;
+        while(fast&&fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
