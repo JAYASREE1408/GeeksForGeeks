4730105f013565baf1a81da7e5b88383c9dda8be
JAYASREE T <jayasreet2112@gmail.com>
2021-03-16 01:52:07 +0530

    Height of Binary Tree

---
+++ Height of Binary Tree.cpp
@@ -0,0 +1,17 @@
+class Solution{
+    public:
+    int height(struct Node* node){
+        if(node==NULL) 
+            return 0;
+        int left=height(node->left);
+        int right=height(node->right);
+        int h;
+        if(left>right){
+            h=1+left;
+        }
+        else{
+            h=1+right;
+        }
+        return h;
+        }
+};
