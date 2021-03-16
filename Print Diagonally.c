fb8651e625db9d72eea5e4e4e0231af24d78119b
JAYASREE T <jayasreet2112@gmail.com>
2021-03-17 00:24:15 +0530

    Print Diagonally

---
+++ Print Diagonally.c
@@ -0,0 +1,71 @@
+
+int main() {
+int t, n ;
+	scanf("%d",&t);
+	while(t--){
+	    scanf("%d",&n);
+	    int arr[n][n] , a , b , l;
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++)
+	        scanf("%d",&arr[i][j]);
+	    }
+	    for(int k=0;k<=2*(n-1);k++){
+	        if(k<n)
+	        l=k;
+	        else
+	        l=n-1;
+	        for(int a=k-l , b=l;a+b==k&&b>=0&&a<n;a++,b--)
+	            printf("%d ",arr[a][b]);
+	    }
+	    printf("\n");
+	}
+
+
+	return 0;
+}
+
+
+
+int main() {
+int t,i,j,k;
+scanf("%d",&t);
+for(k=0;k<t;k++)
+{
+   int n;
+   scanf("%d",&n);
+   int arr[n][n];
+   for(i=0;i<n;i++)
+   {
+       for(j=0;j<n;j++)
+       {
+           scanf("%d",&arr[i][j]);
+       }
+   }
+   int c=0,c1=0;
+   int l=2*n-1;
+  for(i=0;i<l;i++)
+   {
+       if(i<n)
+       {
+           c=0;c1=i;
+           //printf("%d  %d\n",c,c1);
+           for(j=0;j<=i;j++)
+           {
+               printf("%d ",arr[c][c1]);
+               c++;c1--;
+           }
+       }
+       else
+       {
+           c=n-1;c1=i-c;
+           for(j=n-1;j>(i-n);j--)
+           {
+               printf("%d ",arr[c1][c]);
+               c--;c1++;
+           }
+       }
+    }
+   printf("\n");
+}
+return 0;
+}
