295c20c5687f5e44bd61236007278fb0c48bf14c
JAYASREE T <jayasreet2112@gmail.com>
2021-03-17 13:38:17 +0530

    Remove Spaces

---
+++ Remove Spaces.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+  public:
+    string modify (string s)
+    {
+        int i=0;
+        while(s[i]!='\0'){
+            if(s[i]!=' ')
+               cout<<s[i];
+        i++;
+        }
+    }
+};
