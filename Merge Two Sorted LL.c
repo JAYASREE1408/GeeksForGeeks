1ca5e2e8c26822f4426bcbd49e91f874da0c549d
JAYASREE T <jayasreet2112@gmail.com>
2021-03-21 20:43:09 +0530

    Merge Two Sorted LL

---
+++ Merge Two Sorted LL.c
@@ -0,0 +1,21 @@
+Node* sortedMerge(Node* a, Node* b)  
+{  
+  Node* result = NULL;
+ 
+  if (a == NULL) 
+     return(b);
+  else if (b==NULL) 
+     return(a);
+ 
+  if (a->data <= b->data) 
+  {
+     result = a;
+     result->next = sortedMerge(a->next, b);
+  }
+  else
+  {
+     result = b;
+     result->next = sortedMerge(a, b->next);
+  }
+  return(result);
+}  
