8cd92fdbf90376a19ccf520da246f504555d80ff
JAYASREE T <jayasreet2112@gmail.com>
2021-04-24 23:41:42 +0530

    Delete without head pointer

---
+++ Delete without head pointer.c
@@ -0,0 +1,13 @@
+class Solution
+{
+    public:
+    //Function to delete a node without any reference to head pointer.
+    void deleteNode(Node *del)
+    
+    {
+       Node *temp= del->next;
+       *del=*(del->next);
+       delete temp;
+    }
+
+};
